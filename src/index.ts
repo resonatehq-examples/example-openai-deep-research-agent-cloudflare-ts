483d49f5b508ad9b482db44eca5a5110f4ceb9a4
Tomperez98 <tomasperezalvarez@gmail.com>
2025-11-21 11:45:39 -0500

    example app done. missing readme

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,16 @@ import { research } from "./agent";
 
 const resonate = new Resonate();
 
-resonate.register("research", research);
-
-// This is required for Cloudflare Workers, because the only way to access
-// env variables is from the env argument passed to the handler, so we are
-// required to wrap it and set dependencies.
-export default {
-	async fetch(
-		request: Request,
-		env: Record<string, string>,
-		ctx: any,
-	): Promise<Response> {
-		const apiKey = env.OPENAI_API_KEY;
+resonate.onInitialize(async (env: Record<string, string>) => {
+  const apiKey = env.OPENAI_API_KEY;
 		if (!apiKey) {
 			throw new Error("Missing required environment variable: OPENAI_API_KEY");
 		}
 
 		resonate.setDependency("aiclient", new OpenAI({ apiKey }));
+})
 
-		return resonate.handlerHttp().fetch(request, env, ctx);
-	},
-};
+
+resonate.register("research", research);
+
+export default resonate.handlerHttp()
